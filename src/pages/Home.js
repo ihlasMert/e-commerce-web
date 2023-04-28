7332c81a90906b2cb3e8cda1ac144494884b6a90
İhlas Mert <ihlas1mert@gmail.com>
2023-04-28 23:08:46 +0300

    added home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import header from "../../src/styles/home.css"
+import React from "react"
+import home from "../../src/styles/home.css"
+import backImg from "../assets/background-section.png"
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className='homeImage'>
+        <div className='text'>
+          {" "}
+          <h3>25% OFF DİSCOUNT İS RUNNİNG</h3>
+          <h1>Your OneStep Online Grocery Store</h1>
+          <button>Shop Now</button>
+        </div>
+        <img src={backImg} alt='backImg' />
+      </div>
+    </div>
   )
 }
 
