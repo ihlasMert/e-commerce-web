5f190e7ddf530df36cc50806c296f9910f7780a3
İhlas Mert <ihlas1mert@gmail.com>
2023-04-28 22:45:16 +0300

    added footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,48 @@
 import React from "react"
+import "../../styles/footer.css"
 
 const Footer = () => {
-  return (
-    <div className='container'>
-      <div className='row'>
-        <div className='logo'>
-          <h5>FreshGrocery</h5>
-        </div>
-      </div>
-      
+  return <footer className='last'>
+    <div className="logo">
+      <h5>Fresh Groseryy</h5>
     </div>
-  )
+    <div className="input">
+      <h3>Sing up for new products and news and exclusive offers</h3>
+      <input placeholder="Enter Email"/>
+      <button>Sign Up</button>
+    </div>
+    <div className="underline"></div>
+    <div className="companies">
+      <ul>
+        <li>about the company</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+      </ul>
+      <ul>
+        <li>costumer service</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+      </ul>
+      <ul>
+        <li>service</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+      </ul>
+      <ul>
+        <li>about</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+      </ul>
+    </div>
+  </footer>
 }
 
 export default Footer
